357909384d2403acdadf1788559223d766a70c77
maheshraut07 <rautmahesh213@gmail.com>
2024-10-08 00:30:45 +0530

    updated the navbar, footer and herosection

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Button } from './ui/button'
 import { Search } from 'lucide-react'
 import { useDispatch } from 'react-redux';
-import { setSearchText } from '@/redux/jobSlice';
+import { setSearchedQuery } from '@/redux/jobSlice';
 import { useNavigate } from 'react-router-dom';
 
 const HeroSection = () => {
@@ -10,40 +10,32 @@ const HeroSection = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-
     const searchJobHandler = () => {
-        dispatch(setSearchText(query));
+        dispatch(setSearchedQuery(query));
         navigate("/browse");
     }
 
     return (
         <div className='text-center'>
             <div className='flex flex-col gap-5 my-10'>
-                <div className='text-center mx-auto'>
-                    <div className="text-[#F83002] px-4 py-2 rounded-full bg-gray-100 font-medium" >No. 1 Job Hunt Website</div>
-                </div>
-                <div>
-                    <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-                </div>
-                <div>
-                    <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus adipisci cupiditate cum.<br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                <div className='flex w-[40%] shadow-lg border pl-3 border-gray-200 rounded-full items-center gap-4 mx-auto'>
+                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#1A73E8] font-medium'>Your No. 1 Career Companion</span>
+                <h1 className='text-5xl font-bold'>Explore, Apply & <br /> Land Your <span className='text-[#FF5722]'>Perfect Job</span></h1>
+                <p>Unlock new opportunities and pave the path to your success!</p>
+                <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
-                        name="query"
-                        value={query}
+                        placeholder='Discover your perfect career'
                         onChange={(e) => setQuery(e.target.value)}
-                        placeholder="Find your dream jobs"
-                        className="outline-none border-none w-full"
+                        className='outline-none border-none w-full text-gray-700'
                     />
-                    <Button onClick={searchJobHandler} className='rounded-r-full bg-[#6A38C2]'>
-                        <Search className='h-5 w-5' />
+                    <Button onClick={searchJobHandler} className="rounded-r-full bg-[#FF5722]">
+                        <Search className='h-5 w-5 text-white' />
                     </Button>
                 </div>
             </div>
         </div>
     )
+    
 }
 
 export default HeroSection

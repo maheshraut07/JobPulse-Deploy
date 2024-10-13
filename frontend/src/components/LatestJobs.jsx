0f5a723fd1df18e635f52b8bb3c4154cde228b6b
maheshraut07 <rautmahesh213@gmail.com>
2024-10-13 14:19:34 +0530

    made the home page and browse page responsive according to the device viewport size

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import LatestJobCard from './LatestJobCards';
+import LatestJobCard from './LatestJobCard';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux'; 
 
